d91d4dd901e0c12a7bd14b60760d39942a8dd864
Ohans Emmanuel <ohansemmanuel@gmail.com>
2018-12-18 16:08:09 +0100

    complete app demo

--- src/App.js
+++ src/App.js
@@ -1,25 +1,74 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import logo from "./logo.png";
+import "./App.css";
+
+class Chats extends Component {
+  render() {
+    return (
+      <React.Fragment>
+        {this.props.chatList.map((chat, i) => {
+          if (i === 4) {
+            // Simulate an error
+            throw new Error("I crashed!");
+          }
+          return (
+            <li key={i} className="chat-bubble">
+              {chat}
+            </li>
+          );
+        })}
+      </React.Fragment>
+    );
+  }
+}
 
 class App extends Component {
+  chatThreadRef = React.createRef();
+
+  state = {
+    points: 10,
+    chatList: ["Hey", "Hello", "Hi"]
+  };
+
+  _handleAddChat = () => {
+    this.setState(prevState => ({
+      chatList: [...prevState.chatList, "Hello!!!"]
+    }));
+  };
+
+  getSnapshotBeforeUpdate(prevProps, prevState) {
+    if (this.state.chatList > prevState.chatList) {
+      const chatThreadRef = this.chatThreadRef.current;
+      return chatThreadRef.scrollHeight - chatThreadRef.scrollTop;
+    }
+    return null;
+  }
+
+  componentDidUpdate(prevProps, prevState, snapshot) {
+    if (snapshot !== null) {
+      const chatThreadRef = this.chatThreadRef.current;
+      chatThreadRef.scrollTop = chatThreadRef.scrollHeight - snapshot;
+    }
+  }
+
   render() {
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+          <p>You've scored {this.state.points} points.</p>
         </header>
+        <section className="App-chat">
+          <p className="chat-header">
+            Football Chat{" "}
+            <button className="chat-btn" onClick={this._handleAddChat}>
+              Add Chat
+            </button>
+          </p>
+          <ul className="chat-thread" ref={this.chatThreadRef}>
+            <Chats chatList={this.state.chatList} />
+          </ul>
+        </section>
       </div>
     );
   }
