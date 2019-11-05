d11dccb8670e1f86e23fd40869c7d7b9263572ae
Ohans Emmanuel <ohansemmanuel@gmail.com>
2019-11-05 05:09:13 +0100

    complete demo hooks version

--- src/index.js
+++ src/index.js
@@ -1,18 +1,18 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import "./index.css";
-import App from "./App";
-import * as serviceWorker from "./serviceWorker";
-import ErrorBoundary from "./ErrorBoundary";
+import React from 'react'
+import ReactDOM from 'react-dom'
+import './index.css'
+import App2 from './App2'
+import * as serviceWorker from './serviceWorker'
+import ErrorBoundary from './ErrorBoundary'
 
 ReactDOM.render(
   <ErrorBoundary>
-    <App />
+    <App2 />
   </ErrorBoundary>,
-  document.getElementById("root")
-);
+  document.getElementById('root')
+)
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister();
+serviceWorker.unregister()
